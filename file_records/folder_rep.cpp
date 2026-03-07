ec9898cbc47014d2aca3f2f49038824cfa8709c5
mikesinclair66 <michaelkevin.sinclair@gmail.com>
2026-03-06 22:16:24 -0800

    Got rid of dynamic casts because they can be slow

--- file_records/folder_rep.cpp
+++ file_records/folder_rep.cpp
@@ -7,46 +7,41 @@ using namespace file_records;
 
 const vector<string> FolderRep::disincludeFolders = { ".git", "build" };
 
+//takes a root path and lists all nested root paths
+//on each nested path that is a folder, the function is called recursively
+//on each nested path that is a file, a custom operation is performed
 FolderRep* FolderRep::installFolderAtRoot(string root) {
-	vector<FolderRecord*> rootChildren;
+	vector<FolderRecord*> folderChildren;
 	const string name = getRootName(root);
 
 	bool disincludeFolder = false;
-	for (string disincluded : disincludeFolders) {
+	for (string disincluded : disincludeFolders)
 		if(name == disincluded) {
 			disincludeFolder = true;
 			break;
 		}
-	}
 
 	if(!disincludeFolder)
 		for (const auto& entry : filesystem::directory_iterator(root)) {
 			filesystem::path path = entry.path();
 			if (entry.is_regular_file())
-				rootChildren.push_back(new FileRep(path.filename().string(), path.string(),
+				folderChildren.push_back(new FileRep(path.filename().string(), path.string(),
 					path.extension().string()));
 			else
-				rootChildren.push_back(installFolderAtRoot(path.string()));
+				folderChildren.push_back(installFolderAtRoot(path.string()));
 		}
 
-	return new FolderRep(name, root, rootChildren);
+	return new FolderRep(name, root, folderChildren);
 }
 
 void FolderRep::loadAllRootsByExtension(vector<string>& roots, FolderRep* parentFolder,
 const string ext) {
 	for (const auto& child : parentFolder->getChildren()) {
-		FolderRecord::RecordClassifier recordClassifier = child->recordClassifier;
-
-		FileRep* sameChild;
-		switch (recordClassifier) {
-			case FolderRecord::RecordClassifier::FILE:
-				sameChild = static_cast<FileRep*>(child);
-				if (sameChild->getExtension() == ext)
-					roots.push_back(sameChild->getRoot());
-				break;
-			case FolderRecord::RecordClassifier::FOLDER:
-				loadAllRootsByExtension(roots, static_cast<FolderRep*>(child), ext);
-				break;
-		}
+		if(child->recordClassifier == FolderRecord::RecordClassifier::FILE) {
+			FileRep* sameChild = static_cast<FileRep*>(child);
+			if (sameChild->getExtension() == ext)
+				roots.push_back(sameChild->getRoot());
+		} else 
+			loadAllRootsByExtension(roots, static_cast<FolderRep*>(child), ext);
 	}
 }
